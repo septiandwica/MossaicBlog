64a3f7ff6e28c61d1d0fa6ebe8bc52db3662b705
septiandwica <septiandwica03@gmail.com>
2024-04-18 01:04:09 +0700

    copyright update

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,8 +1,8 @@
 <footer id="footer" class="footer">
   <div class="copyright">
-      &copy; {{date('Y')}} Copyright  <strong>BlogAdmin</strong>. All Rights Reserved.
+      &copy; {{date('Y')}} Copyright  <strong>MossaicBlog</strong>. All Rights Reserved.
   </div>
   <div class="credits">
-      Developed and Maintenced by <a href=""><strong>TianWebCode</strong></a>
+      Developed and Maintenced by <a href="https://tiancode.my.id"><strong>TianCode</strong></a>
   </div>
 </footer>
