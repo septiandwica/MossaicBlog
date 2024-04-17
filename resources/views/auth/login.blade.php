fcbbf095b6078c6d4dd313e1a2ca32193a612cea
septiandwica <septiandwica03@gmail.com>
2024-04-18 01:02:32 +0700

    fix bug

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,17 +23,17 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="{{ asset('Admin/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-  <link href="{{ asset('Admin/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
-  <link href="{{ asset('Admin/assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
-  <link href="{{ asset('Admin/assets/vendor/quill/quill.snow.css')}}" rel="stylesheet">
-  <link href="{{ asset('Admin/assets/vendor/quill/quill.bubble.css')}}" rel="stylesheet">
-  <link href="{{ asset('Admin/assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
-  <link href="{{ asset('Admin/assets/vendor/simple-datatables/style.css')}}" rel="stylesheet">
+  <link href="{{ asset('admin/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{ asset('admin/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{ asset('admin/assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+  <link href="{{ asset('admin/assets/vendor/quill/quill.snow.css')}}" rel="stylesheet">
+  <link href="{{ asset('admin/assets/vendor/quill/quill.bubble.css')}}" rel="stylesheet">
+  <link href="{{ asset('admin/assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
+  <link href="{{ asset('admin/assets/vendor/simple-datatables/style.css')}}" rel="stylesheet">
 
   
   <!-- Template Main CSS File -->
-  <link href="{{ asset('Admin/assets/css/style.css')}}" rel="stylesheet">
+  <link href="{{ asset('admin/assets/css/style.css')}}" rel="stylesheet">
 
 </head>
 
@@ -113,17 +113,17 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="{{ asset('Admin/assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
-  <script src="{{ asset('Admin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{ asset('Admin/assets/vendor/chart.js/chart.umd.js')}}"></script>
-  <script src="{{ asset('Admin/assets/vendor/echarts/echarts.min.js')}}"></script>
-  <script src="{{ asset('Admin/assets/vendor/quill/quill.min.js')}}"></script>
-  <script src="{{ asset('Admin/assets/vendor/simple-datatables/simple-datatables.js')}}"></script>
-  <script src="{{ asset('Admin/assets/vendor/tinymce/tinymce.min.js')}}"></script>
-  <script src="{{ asset('Admin/assets/vendor/php-email-form/validate.js')}}"></script>
+  <script src="{{ asset('admin/assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
+  <script src="{{ asset('admin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{ asset('admin/assets/vendor/chart.js/chart.umd.js')}}"></script>
+  <script src="{{ asset('admin/assets/vendor/echarts/echarts.min.js')}}"></script>
+  <script src="{{ asset('admin/assets/vendor/quill/quill.min.js')}}"></script>
+  <script src="{{ asset('admin/assets/vendor/simple-datatables/simple-datatables.js')}}"></script>
+  <script src="{{ asset('admin/assets/vendor/tinymce/tinymce.min.js')}}"></script>
+  <script src="{{ asset('admin/assets/vendor/php-email-form/validate.js')}}"></script>
 
   <!-- Template Main JS File -->
-  <script src="{{ asset('Admin/assets/js/main.js')}}"></script>
+  <script src="{{ asset('admin/assets/js/main.js')}}"></script>
 
 </body>
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -23,17 +23,17 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="{{ asset('Admin/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-  <link href="{{ asset('Admin/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
-  <link href="{{ asset('Admin/assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
-  <link href="{{ asset('Admin/assets/vendor/quill/quill.snow.css')}}" rel="stylesheet">
-  <link href="{{ asset('Admin/assets/vendor/quill/quill.bubble.css')}}" rel="stylesheet">
-  <link href="{{ asset('Admin/assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
-  <link href="{{ asset('Admin/assets/vendor/simple-datatables/style.css')}}" rel="stylesheet">
+  <link href="{{ asset('admin/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{ asset('admin/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{ asset('admin/assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+  <link href="{{ asset('admin/assets/vendor/quill/quill.snow.css')}}" rel="stylesheet">
+  <link href="{{ asset('admin/assets/vendor/quill/quill.bubble.css')}}" rel="stylesheet">
+  <link href="{{ asset('admin/assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
+  <link href="{{ asset('admin/assets/vendor/simple-datatables/style.css')}}" rel="stylesheet">
 
   
   <!-- Template Main CSS File -->
-  <link href="{{ asset('Admin/assets/css/style.css')}}" rel="stylesheet">
+  <link href="{{ asset('admin/assets/css/style.css')}}" rel="stylesheet">
 
 </head>
 
@@ -60,7 +60,7 @@
 
                     <div class="pt-4 pb-2">
                         @if (App\Models\User::count() === 0)
-                            <h5 class="card-title text-center pb-0 fs-4">Admin Registration</h5>
+                            <h5 class="card-title text-center pb-0 fs-4">admin Registration</h5>
                             <p class="text-center small">Enter your personal details to create admin account</p>
                         @else
                             <h5 class="card-title text-center pb-0 fs-4">Create Account</h5>
@@ -147,17 +147,17 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="{{ asset('Admin/assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
-  <script src="{{ asset('Admin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{ asset('Admin/assets/vendor/chart.js/chart.umd.js')}}"></script>
-  <script src="{{ asset('Admin/assets/vendor/echarts/echarts.min.js')}}"></script>
-  <script src="{{ asset('Admin/assets/vendor/quill/quill.min.js')}}"></script>
-  <script src="{{ asset('Admin/assets/vendor/simple-datatables/simple-datatables.js')}}"></script>
-  <script src="{{ asset('Admin/assets/vendor/tinymce/tinymce.min.js')}}"></script>
-  <script src="{{ asset('Admin/assets/vendor/php-email-form/validate.js')}}"></script>
+  <script src="{{ asset('admin/assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
+  <script src="{{ asset('admin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{ asset('admin/assets/vendor/chart.js/chart.umd.js')}}"></script>
+  <script src="{{ asset('admin/assets/vendor/echarts/echarts.min.js')}}"></script>
+  <script src="{{ asset('admin/assets/vendor/quill/quill.min.js')}}"></script>
+  <script src="{{ asset('admin/assets/vendor/simple-datatables/simple-datatables.js')}}"></script>
+  <script src="{{ asset('admin/assets/vendor/tinymce/tinymce.min.js')}}"></script>
+  <script src="{{ asset('admin/assets/vendor/php-email-form/validate.js')}}"></script>
 
   <!-- Template Main JS File -->
-  <script src="{{ asset('Admin/assets/js/main.js')}}"></script>
+  <script src="{{ asset('admin/assets/js/main.js')}}"></script>
 
 </body>
 
